b82b479c8e10ae288b9dddead86a5ea131eaa65b
Nassim LOUNADI <nassimlnd37@gmail.com>
2022-11-10 19:23:20 +0100

    ✨️ Adding some animations !

--- js/index.js
+++ js/index.js
@@ -1,5 +1,20 @@
 function closeWindow() {
     let app = document.getElementById("app");
+    app.classList = "fade-out-down";
+    setTimeout(() => {
+        app.classList = "";
+        app.style.display = "none";
+    }, 500);
+}
 
-    app.style.display = "none";
+function openWindow(appName) {
+    let app = document.getElementById(appName);
+    let appButton = appName + "-button";
+    let dockButton = document.getElementById(appName + "-button");
+
+    console.log(appButton);
+
+    dockButton.classList = "app-icon bounce";
+    setTimeout(() => {  dockButton.classList = "app-icon";
+        app.style.display = "flex";}, 1000);
 }
